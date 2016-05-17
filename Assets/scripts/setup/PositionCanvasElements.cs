a6814ea756a682e818d1aa81a783e3d3db045ef9
SirGFM <gfmandaji@hotmail.com>
2016-05-16 22:55:18 -0300

    Implement scaling
    Required game objects are now automatically searched

--- Assets/scripts/setup/PositionCanvasElements.cs
+++ Assets/scripts/setup/PositionCanvasElements.cs
@@ -4,31 +4,39 @@ using System.Collections;
 
 public class PositionCanvasElements : MonoBehaviour {
 
-	/** Game object that represents the game view (i.e., canvas object
-	 * un which the game camera will be displayed) */
-	public RectTransform gameView;
-
-	/** The panel at the right side of the screen, which may be shrunk to
-	 * free up space to the game view, if necessary */
-	public RectTransform panel;
-
 	/** Initialize the game view in a resolution independent way */
 	void Start () {
+		/** Game object that represents the game view (i.e., canvas object
+	 	 * un which the game camera will be displayed) */
+		RectTransform gameView;
+		/** The panel at the right side of the screen, which may be shrunk to
+	 	 * free up space to the game view, if necessary */
+		RectTransform panel;
 		/** The input image */
 		RawImage image;
 		/** Central position of the texture within the canvas */
 		Vector2 textureCenter;
+		/** Factor by which each pixel is scaled */
+		float zoom;
+		float zoomx, zoomy;
 		/** Half the dimension of the texture */
 		int imageHalfWidth;
 
+		gameView = GameObject.Find("Game View").GetComponent<RectTransform>();
+		panel = GameObject.Find("Game Status").GetComponent<RectTransform>();
+
 		/* Calculate how much is half the texture's width,
 		 * to check if it would fit at the canvas' left half */
-		image = this.gameView.GetComponent<RawImage>();
+		image = gameView.GetComponentInChildren<RawImage>();
 		imageHalfWidth = (int)Mathf.Floor(image.texture.width * 0.5f);
 		/* Calculate the texture position so it stays at the
 		 * center of the canvas' left half */
 		textureCenter = new Vector2(Camera.main.pixelWidth * 0.25f,
 				Camera.main.pixelHeight * 0.5f);
+
+		zoomx = (Camera.main.pixelWidth * 0.5f) / image.texture.width;
+		zoomy = Camera.main.pixelHeight / image.texture.height;
+		zoom = Mathf.Floor(Mathf.Min(zoomx, zoomy));
 
 		/* If the image wouldn't fit on the left half,
 		 * push the status panel to the right */
@@ -37,19 +45,19 @@ public class PositionCanvasElements : MonoBehaviour {
 
 			textureCenter.x = imageHalfWidth;
 
-			pos = new Vector2(image.texture.width / Camera.main.pixelWidth,
+			pos = new Vector2(image.texture.width * zoom / Camera.main.pixelWidth,
 					panel.anchorMin.y);
 			panel.anchorMin = pos;
 		}
 
 		/* Set the image position */
-		this.gameView.anchorMin = Vector2.zero;
-		this.gameView.anchorMax = Vector2.zero;
-		this.gameView.anchoredPosition = textureCenter;
+		gameView.anchorMin = Vector2.zero;
+		gameView.anchorMax = Vector2.zero;
+		gameView.anchoredPosition = textureCenter;
 
 		/* Calculate the zoom factor */
 		/* Set the image dimensions */
-		this.gameView.sizeDelta = new Vector2(image.texture.width,
-				image.texture.height);
+		gameView.sizeDelta = zoom * (new Vector2(image.texture.width,
+				image.texture.height));
 	}
 }
