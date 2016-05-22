604a8937e11bf333990fe1d769631de5bdf2913f
SirGFM <gfmandaji@hotmail.com>
2016-05-22 11:35:32 -0300

    Add component to spawn an explosion whenever GOs are disabled

---
+++ Assets/scripts/objects/fx/ExplodeOnDisable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeOnDisable : MonoBehaviour {
+
+	void OnDisable() {
+		Vector3 pos;
+
+		pos = this.transform.position;
+		if (Mathf.Abs(pos.x) < Global.width && Mathf.Abs(pos.y) < Global.height) {
+			GameObject go;
+
+			go = Global.explosionRecycler.recycle();
+			go.transform.position = pos;
+		}
+	}
+}
