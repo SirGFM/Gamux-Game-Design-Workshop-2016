a39d4e105c806dc863f2d843e96b5920c20a7049
SirGFM <gfmandaji@hotmail.com>
2016-05-22 19:14:16 -0300

    Add component to get score when grazing bullets

---
+++ Assets/scripts/objects/collision/HitboxScoreOnGraze.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitboxScoreOnGraze : Hitbox {
+
+	/** How much score the player gets per second
+	 * when grazing something */
+	public float scoreOnGraze = 6.0f;
+
+	protected override void onGraze (Hitbox other) {
+		Global.score.increase(this.scoreOnGraze * Time.fixedDeltaTime);
+	}
+}
