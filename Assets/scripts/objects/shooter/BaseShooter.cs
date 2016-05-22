1768cf9886c6c828630564b6faadfa4361c184a5
SirGFM <gfmandaji@hotmail.com>
2016-05-22 04:08:29 -0300

    Add basic bullet and component to shoot it

---
+++ Assets/scripts/objects/shooter/BaseShooter.cs
@@ -0,0 +1,49 @@
+﻿using Color = UnityEngine.Color;
+using MonoBehaviour = UnityEngine.MonoBehaviour;
+using Vector3 = UnityEngine.Vector3;
+using Handles = UnityEditor.Handles;
+
+public abstract class BaseShooter : MonoBehaviour {
+
+	/** Radius of the offset */
+	public const float radius = 0.05f;
+
+	/** Color of the hitbox */
+	public Color color = Color.white;
+	
+	/** Offset of the hitbox from the gameObject's center */
+	public Vector3 offset;
+
+	/** Number of bullets shot at once */
+	public int count = 1;
+
+	/** Recycler used to spawn bullets */
+	public Recycler recycler;
+
+	/** Opening angle, used when shooting multiple
+	 * bullets toward the forward angle */
+	public float angleRange = 60.0f;
+
+	/**
+	 * Override this function to implement the component's
+	 * shooting behaviour. It then should be called by the
+	 * component used to control *when* to shoot.
+	 *
+	 * @param  [ in]angle Direction toward which bullets
+	 *                    should be shot (0º is to the rigth, 90º is upward)
+	 */
+	public abstract void shoot(float angle);
+
+	/* Draw the actual hitbox on the editor */
+	void OnDrawGizmos() {
+		Color original;
+
+		original = UnityEditor.Handles.color;
+		Handles.color = this.color;
+
+		Handles.DrawWireDisc(this.transform.position + this.offset,
+				Vector3.back, BaseShooter.radius);
+
+		Handles.color = original;
+	}
+}
---
+++ Assets/scripts/objects/shooter/BasicShooter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicShooter : BaseShooter {
+
+	public override void shoot (float angle) {
+		float deltaAngle, curAngle;
+		int i;
+
+		/* Calculate the initial angle and its variation between bullets */
+		if (this.count > 1) {
+			deltaAngle = this.angleRange / (float)(this.count - 1);
+			curAngle = angle - this.angleRange * 0.5f;
+		}
+		else {
+			deltaAngle = 0.0f;
+			curAngle = angle;
+		}
+
+		i = 0;
+		while (i < this.count) {
+			GameObject go;
+			ConstantMovement constMove;
+
+			go = this.recycler.recycle();
+			go.transform.position = this.offset + this.transform.position;
+
+			constMove = go.GetComponent<ConstantMovement>();
+
+			if (constMove != null) {
+				constMove.angle = curAngle;
+			}
+
+			i++;
+			curAngle += deltaAngle;
+		}
+	}
+}
