9b8b4b65551fe13b6286505403824f09191d18c6
SirGFM <gfmandaji@hotmail.com>
2016-05-22 09:39:21 -0300

    Add component to perfectly follow (and hit, if fast enough) another gameObject

---
+++ Assets/scripts/objects/movement/FollowTarget.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : BaseMovement {
+
+	/** GameObject that is being targeted */
+	public BaseMovement target;
+
+	protected override void fixedUpdate () {
+		/* Set the velocity so the GO follows were its
+		 * target is gonna be on the next turn */
+		this.velocity = (this.target.position + this.target.velocity * Time.deltaTime) - this.position;
+	}
+
+}
